6966c597ddde2448a11cf8463a7efbf34e15fbc1
Sam Bishop <sambishop3015@hotmail.com>
2020-09-25 00:36:56 -0400

    JS Completed

--- script.js
+++ script.js
@@ -1,14 +1,122 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+// DOM Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
+// Objects
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generate Password Click Events
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// Copy To Clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password Copied to Clipboard');
+})
+
+// Generate Password Function
+function generatePassword(lower, upper, number, symbol, length) {
+    //1. Init Password Variable
+    //2. Filter Out Unchecked Types
+    //3. Loop Over Length, Call generator Function for Each Type
+    //4. Add Final Password to PW Variable and Return
+
+    let generatePassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    // console.log('typesCount: ', typesCount);
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    // console.log('typesArr: ', typesArr);
+    
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            // console.log('funcName: ', funcName);
+
+            generatePassword += randomFunc[funcName] ();
+        });
+    }
+
+    const finalPassword = (generatePassword.slice(0, length));
+    return finalPassword;
+}
+
+// Generator Functions
+
+// Random Lower Letter Function - http://www.net-comber.com/charset.html
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+// Random Upper Letter Function
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+
+// Random Number Function
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+// Random Symbols Function
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// console.log(getRandomSymbol());
